1b62e36a3e5affda8435ee77352ca4d8441b802d
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2014-07-13 13:31:40 +0700

    Cleanup

--- travel-request/travel/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from travel.request.flows import RequestFlow
-
-urlpatterns = patterns('',  # NOQA
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(RequestFlow.instance.urls)),
-    url(r'^', include('travel.website'))
-)
